1769e09c4ef872d7d280da3f0739a3028720d14a
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-09 19:00:33 +0700

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,13 @@
+public class Day2 {
+
+   
+    public static void main(String[] args) {
+        String a = "Alif";
+        int b = 18;
+        System.out.print(a+"\n");
+        System.out.println("D0223517");
+        System.out.printf("umur = %d \n",b);
+        
+    }
+    
+}
