072bcf2eff3beacbe14bf3cd8400d98bdd7cb016
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-10 15:23:29 +0700

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,20 @@
+package day3.java;
+
+
+public class Day3Java {
+
+   
+    public static void main(String[] args) {
+        // Membuat variabel bertipe String
+        String nama = "Alip"; // Menyimpan nama seseorang
+        String salam = "Halo, "; // Menyimpan kata sapaan
+
+        // Menggabungkan dua string
+        String pesan = salam + nama;
+
+        // Menampilkan hasil ke layar
+        System.out.println(pesan); // Output: Halo, Alip
+
+    }
+    
+}
