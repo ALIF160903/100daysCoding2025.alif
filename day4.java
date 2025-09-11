7d4c5b8d6472e5f033c6251ea7e2a7307c32a05a
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-11 18:54:50 +0700

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,18 @@
+
+package day4.java;
+
+public class Day4Java {
+
+    public static void main(String[] args) {
+       String a ="Alif";
+       String b ="Majene";
+       int c = 21;
+       System.out.print("Bio Data\n");
+       // \t untuk tab, \n untuk baris baru
+       System.out.println("Nama \t: "+a+"\nAlamat \t: "+b+"\numur \t: "+c+" thn");//menggunkan println
+       
+       System.out.printf("\nNama \t: %s\nAlmat \t: %s\nUmur\t: %d\n",a,b,c);//menggunakan printf
+
+    }
+    
+}
