41619fd115c36fe30afa9464ca89f703ada43994
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-12 17:06:31 +0700

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,15 @@
+package day5;
+public class Day5 {
+    public static void main(String[] args) {
+        byte a = 10;       // 8-bit
+        short b = 200;     // 16-bit
+        int c = 1000;      // 32-bit
+        long d = 100000L;  // 64-bit
+
+        System.out.println("byte  : " + a);
+        System.out.println("short : " + b);
+        System.out.println("int   : " + c);
+        System.out.println("long  : " + d);
+    }
+    
+}
