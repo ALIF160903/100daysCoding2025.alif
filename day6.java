188e77e9b2e45c7ab60dd7241a48cd695149bd85
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-13 18:19:53 +0700

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,13 @@
+package day6;
+public class Day6 {
+    public static void main(String[] args) {
+       
+     float a = 1.7f;
+     double b = 198.3;
+
+     System.out.println("Nilai float : " + a);
+     System.out.println("Nilai double : " + b);
+   }
+}
+    
+    
