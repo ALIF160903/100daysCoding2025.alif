75a0d38aa7dd0e4889d0f9f4de4ecbe04ec3696b
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-16 12:31:38 +0700

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,9 @@
+public class VariabelPalingSederhana {
+    public static void main(String[] args) {
+        int angka = 10; // deklarasi + inisialisasi
+        System.out.println(angka);
+
+        angka = 20; // update
+        System.out.println(angka);
+    }
+}
