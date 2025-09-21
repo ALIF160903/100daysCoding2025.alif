92dd087942e1cc727d09644709a3e668c1d4a1d7
ALIF160903 <muhalifsatria09@gmail.com>
2025-09-21 20:15:40 +0700

    Create day14.java

---
+++ day14.java
@@ -0,0 +1,32 @@
+
+package day14;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+       
+        String a = "123";
+        String x = "True";
+        int b = Integer.parseInt(a);
+        byte c = Byte.parseByte(a);
+        long d = Long.parseLong(a);
+        short e = Short.parseShort(a);
+        float f = Float.parseFloat(a);
+        double g = Double.parseDouble(a);
+        char h = a.charAt(2);
+        boolean i = Boolean.parseBoolean(x);
+        System.out.println(a);
+        System.out.println("Diubah ke int : " + b);
+        System.out.println("Diubah ke byte : " + c);
+        System.out.println("Diubah ke long : " + d);
+        System.out.println("Diubah ke short : " + e);
+        System.out.println("Diubah ke float : " + f);
+        System.out.println("Diubah ke double : " + g);
+        System.out.println("Diubah ke char : " + h);
+        System.out.println("Diubah ke boolean : " + i);
+
+    }
+}
+    
+    
+
